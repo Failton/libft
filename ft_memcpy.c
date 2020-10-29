05e013b18efb264b09b5c4c22962d68b13e07f0e
Failton <ilya.zig.good@gmail.com>
2020-10-29 19:12:46 +0300

    memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+void *memcpy(void *dest, const void *src, size_t n)
+{
+	int		i;
+	unsigned char	*str;
+	unsigned char	*src2;
+
+	i = 0;
+	str = dest;
+	src2 = src;
+	while (n)
+	{
+		str[i] = src2[i];
+		i++;
+		n--;
+	}
+	return (str);
+}
+
+int main()
+{
+	char a[15] = "1234567890";
+	char b[10] = "qwertyuiop";
+	printf("%s %s\n", a, b);
+	memcpy(a, b, 5);
+	printf("%s %s\n", a, b);
+}
